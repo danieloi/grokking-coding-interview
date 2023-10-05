4b4fcb65b9fb1c2332bb183ef8b088886205725f
Mayowa Daniel <mayowadan17@gmail.com>
2023-10-05 09:35:24 +0100

    format code and add comments

--- ts/4-merge-intervals/01-merge-intervals.ts
+++ ts/4-merge-intervals/01-merge-intervals.ts
@@ -1,73 +1,81 @@
 import { Interval } from "./utils";
 
+// Function to find the intersecting points between two intervals
 function mergeIntervals(intervals: Interval[]) {
-    if (!intervals.length) return;
+  if (!intervals.length) return;
 
-    let result: Interval[] = [];
+  let result: Interval[] = [];
 
-    result.push(new Interval(intervals[0].start, intervals[0].end));
+  // Adding the first pair in the result list from the input list
+  result.push(new Interval(intervals[0].start, intervals[0].end));
 
-    let lastAddedInterval, curStart, curEnd, prevEnd;
-    for (let i = 1; i < intervals.length; i++) {
-        lastAddedInterval = result[result.length - 1];
+  let lastAddedInterval, curStart, curEnd, prevEnd;
+  for (let i = 1; i < intervals.length; i++) {
+    // Getting the recent added interval in the result list
+    lastAddedInterval = result[result.length - 1];
 
-        curStart = intervals[i].start;
-        curEnd = intervals[i].end;
-        prevEnd = lastAddedInterval.end;
-    
-        if (prevEnd >= curStart) {// we have an overlap
-            result[result.length - 1].end = Math.max(curEnd, prevEnd);
-        } else { // no overlap
-            result.push(new Interval(curStart, curEnd));
-        }
+    // Getting and initializing input pair
+    curStart = intervals[i].start;
+    curEnd = intervals[i].end;
+
+    // Getting the ending timestamp of the previous interval
+    prevEnd = lastAddedInterval.end;
+
+    if (prevEnd >= curStart) {
+      // we have an overlap
+      result[result.length - 1].end = Math.max(curEnd, prevEnd);
+    } else {
+      // no overlap
+      result.push(new Interval(curStart, curEnd));
     }
-    return result;
+  }
+  return result;
 }
 
 // Driver code
 // Helper function to display intervals
 function intervalListToStr(list) {
-    let s = "[";
-    for (let i = 0; i < list.length; i++) {
-        s += list[i].formatInterval();
-        if (i != list.length - 1) s += ", ";
-    }
-    s += "]";
-    return s;
+  let s = "[";
+  for (let i = 0; i < list.length; i++) {
+    s += list[i].formatInterval();
+    if (i != list.length - 1) s += ", ";
+  }
+  s += "]";
+  return s;
 }
 
 function main() {
-    let v1 = [new Interval(1, 5), new Interval(3, 7), new Interval(4, 6)],
-        v2 = [
-            new Interval(1, 5),
-            new Interval(4, 6),
-            new Interval(6, 8),
-            new Interval(11, 15),
-        ],
-        v3 = [
-            new Interval(3, 7),
-            new Interval(6, 8),
-            new Interval(10, 12),
-            new Interval(11, 15),
-        ],
-        v4 = [new Interval(1, 5)],
-        v6 = [new Interval(1, 9), new Interval(3, 8), new Interval(4, 4)],
-        v7 = [new Interval(1, 2), new Interval(3, 4), new Interval(8, 8)],
-        v8 = [new Interval(1, 5), new Interval(1, 3)],
-        v9 = [new Interval(1, 5), new Interval(6, 9)],
-        v10 = [new Interval(0, 0), new Interval(1, 18), new Interval(1, 3)];
+  let v1 = [new Interval(1, 5), new Interval(3, 7), new Interval(4, 6)],
+    v2 = [
+      new Interval(1, 5),
+      new Interval(4, 6),
+      new Interval(6, 8),
+      new Interval(11, 15),
+    ],
+    v3 = [
+      new Interval(3, 7),
+      new Interval(6, 8),
+      new Interval(10, 12),
+      new Interval(11, 15),
+    ],
+    v4 = [new Interval(1, 5)],
+    v6 = [new Interval(1, 9), new Interval(3, 8), new Interval(4, 4)],
+    v7 = [new Interval(1, 2), new Interval(3, 4), new Interval(8, 8)],
+    v8 = [new Interval(1, 5), new Interval(1, 3)],
+    v9 = [new Interval(1, 5), new Interval(6, 9)],
+    v10 = [new Interval(0, 0), new Interval(1, 18), new Interval(1, 3)];
 
-    let allIntervals = [v1, v2, v3, v4, v6, v7, v8, v9, v10];
+  let allIntervals = [v1, v2, v3, v4, v6, v7, v8, v9, v10];
 
-    for (let i = 0; i < allIntervals.length; i++) {
-        console.log(
-            i + 1 + ". Intervals to merge:",
-            intervalListToStr(allIntervals[i])
-        );
-        let result = mergeIntervals(allIntervals[i]);
-        console.log("   Merged intervals:", intervalListToStr(result));
-        console.log("-".repeat(100));
-    }
+  for (let i = 0; i < allIntervals.length; i++) {
+    console.log(
+      i + 1 + ". Intervals to merge:",
+      intervalListToStr(allIntervals[i])
+    );
+    let result = mergeIntervals(allIntervals[i]);
+    console.log("   Merged intervals:", intervalListToStr(result));
+    console.log("-".repeat(100));
+  }
 }
 
 main();
--- ts/4-merge-intervals/utils.ts
+++ ts/4-merge-intervals/utils.ts
@@ -1,24 +1,24 @@
 class Interval {
-    start: number;
-    end: number;
-    closed: boolean;
-    setClosed: (closed: boolean) => void;
-    formatInterval: () => string;
-    constructor(start, end) {
-        this.start = start;
-        this.end = end;
-        this.closed = true; // the interval is closed by default
+  start: number;
+  end: number;
+  closed: boolean;
+  setClosed: (closed: boolean) => void;
+  formatInterval: () => string;
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+    this.closed = true; // the interval is closed by default
 
-        this.setClosed = function (closed) {
-            this.closed = closed;
-        };
+    this.setClosed = function (closed) {
+      this.closed = closed;
+    };
 
-        this.formatInterval = function () {
-            return this.closed
-                ? "[" + this.start + ", " + this.end + "]"
-                : "(" + this.start + ", " + this.end + ")";
-        };
-    }
+    this.formatInterval = function () {
+      return this.closed
+        ? "[" + this.start + ", " + this.end + "]"
+        : "(" + this.start + ", " + this.end + ")";
+    };
+  }
 }
 
 export { Interval };
