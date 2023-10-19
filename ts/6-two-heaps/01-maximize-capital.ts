7532f53b176da2aa774925f0e88f162041520321
Mayowa Daniel <mayowadan17@gmail.com>
2023-10-19 11:00:46 +0100

    add maximize capital problem

---
+++ ts/6-two-heaps/01-maximize-capital.ts
@@ -0,0 +1,76 @@
+import { MaxHeap, MinHeap } from "./utils";
+
+function maximumCapital(c, k, capitals, profits) {
+  let currentCapital = c;
+  let capitalsMinHeap = new MinHeap();
+  let profitsMaxHeap = new MaxHeap();
+
+  // Insert all capitals values to a min-heap
+  for (var x = 0; x < capitals.length; x++) {
+    capitalsMinHeap.push([capitals[x], x]);
+  }
+
+  // Calculate capital of all the required number of projects
+  // containing maximum profit
+  for (var counter = 0; counter < k; counter++) {
+    // Select projects (in the range of current capital)
+    // then push them in max-heap
+    while (
+      capitalsMinHeap.size() > 0 &&
+      capitalsMinHeap.peek()[0] <= currentCapital
+    ) {
+      let element = capitalsMinHeap.pop();
+      c = element[0];
+      let i = element[1];
+      profitsMaxHeap.push([profits[i], i]);
+    }
+
+    // check if the max-heap is empty
+    if (profitsMaxHeap.size() == 0) {
+      break;
+    }
+
+    // Select those projects from max-heap that contain maximum profit
+    let element = profitsMaxHeap.pop();
+    let j = element[0];
+    console.log("\t\tUpdated capital =", currentCapital, "+", j);
+    currentCapital = currentCapital + j;
+  }
+
+  return currentCapital;
+}
+
+function main() {
+  const input = [
+    [0, 1, [1, 1, 2], [1, 2, 3]],
+    [1, 2, [1, 2, 2, 3], [2, 4, 6, 8]],
+    [2, 3, [1, 3, 4, 5, 6], [1, 2, 3, 4, 5]],
+    [1, 3, [1, 2, 3, 4], [1, 3, 5, 7]],
+    [7, 2, [6, 7, 8, 10], [4, 8, 12, 14]],
+    [2, 4, [2, 3, 5, 6, 8, 12], [1, 2, 5, 6, 8, 9]],
+  ];
+  let num = 1;
+  for (var i = 0; i < input.length; i++) {
+    console.log(
+      i + 1 + ".\tProject capital requirements:\t",
+      input[i][2]
+    );
+    console.log("\tProject expected profits:\t", input[i][3]);
+    console.log("\tNumber of projects:\t\t", input[i][1]);
+    console.log("\tStart-up capital:\t\t", input[i][0]);
+    console.log("\n\t\tProcessing:");
+    console.log(
+      "\n\tMaximum capital earned:",
+      maximumCapital(
+        input[i][0],
+        input[i][1],
+        input[i][2],
+        input[i][3]
+      )
+    );
+    console.log("-".repeat(100));
+    num += 1;
+  }
+}
+
+main();
---
+++ ts/6-two-heaps/utils.ts
@@ -0,0 +1,215 @@
+export class MaxHeap {
+  data: any[];
+  compareVal: (a: any, b: any) => number;
+  constructor(data = new Array()) {
+    this.data = data;
+    this.compareVal = (a, b) => b - a; // Invert the comparison for MaxHeap
+    this.heapify();
+  }
+
+  heapify() {
+    if (this.size() < 2) {
+      return;
+    }
+    for (let i = 1; i < this.size(); i++) {
+      this.percolateUp(i);
+    }
+  }
+
+  peek() {
+    if (this.size() === 0) {
+      return null;
+    }
+    return this.data[0];
+  }
+
+  push(value) {
+    this.data.push(value);
+    this.percolateUp(this.size() - 1);
+  }
+
+  pop() {
+    if (this.size() === 0) {
+      return null;
+    }
+    const result = this.data[0];
+    const last = this.data.pop();
+    if (this.size() !== 0) {
+      this.data[0] = last;
+      this.percolateDown(0);
+    }
+    return result;
+  }
+
+  percolateUp(index) {
+    while (index > 0) {
+      const parentIndex = (index - 1) >> 1;
+      if (
+        this.compareVal(
+          this.data[index][0],
+          this.data[parentIndex][0]
+        ) < 0
+      ) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  percolateDown(index) {
+    const lastIndex = this.size() - 1;
+    while (true) {
+      const leftIndex = index * 2 + 1;
+      const rightIndex = index * 2 + 2;
+      let findIndex = index;
+
+      if (
+        leftIndex <= lastIndex &&
+        this.compareVal(
+          this.data[leftIndex][0],
+          this.data[findIndex][0]
+        ) < 0
+      ) {
+        findIndex = leftIndex;
+      }
+
+      if (
+        rightIndex <= lastIndex &&
+        this.compareVal(
+          this.data[rightIndex][0],
+          this.data[findIndex][0]
+        ) < 0
+      ) {
+        findIndex = rightIndex;
+      }
+
+      if (index !== findIndex) {
+        this.swap(index, findIndex);
+        index = findIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  swap(index1, index2) {
+    [this.data[index1], this.data[index2]] = [
+      this.data[index2],
+      this.data[index1],
+    ];
+  }
+
+  size() {
+    return this.data.length;
+  }
+}
+
+export class MinHeap {
+  data: any[];
+  compareVal: (a: any, b: any) => number;
+  constructor(data = new Array()) {
+    this.data = data;
+    this.compareVal = (a, b) => a - b;
+    this.heapify();
+  }
+
+  heapify() {
+    if (this.size() < 2) {
+      return;
+    }
+    for (let i = 1; i < this.size(); i++) {
+      this.percolateUp(i);
+    }
+  }
+
+  peek() {
+    if (this.size() === 0) {
+      return null;
+    }
+    return this.data[0];
+  }
+
+  push(value) {
+    this.data.push(value);
+    this.percolateUp(this.size() - 1);
+  }
+
+  pop() {
+    if (this.size() === 0) {
+      return null;
+    }
+    const result = this.data[0];
+    const last = this.data.pop();
+    if (this.size() !== 0) {
+      this.data[0] = last;
+      this.percolateDown(0);
+    }
+    return result;
+  }
+
+  percolateUp(index) {
+    while (index > 0) {
+      const parentIndex = (index - 1) >> 1;
+      if (
+        this.compareVal(
+          this.data[index][0],
+          this.data[parentIndex][0]
+        ) < 0
+      ) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  percolateDown(index) {
+    const lastIndex = this.size() - 1;
+    while (true) {
+      const leftIndex = index * 2 + 1;
+      const rightIndex = index * 2 + 2;
+      let findIndex = index;
+
+      if (
+        leftIndex <= lastIndex &&
+        this.compareVal(
+          this.data[leftIndex][0],
+          this.data[findIndex][0]
+        ) < 0
+      ) {
+        findIndex = leftIndex;
+      }
+
+      if (
+        rightIndex <= lastIndex &&
+        this.compareVal(
+          this.data[rightIndex][0],
+          this.data[findIndex][0]
+        ) < 0
+      ) {
+        findIndex = rightIndex;
+      }
+
+      if (index !== findIndex) {
+        this.swap(index, findIndex);
+        index = findIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  swap(index1, index2) {
+    [this.data[index1], this.data[index2]] = [
+      this.data[index2],
+      this.data[index1],
+    ];
+  }
+
+  size() {
+    return this.data.length;
+  }
+}
