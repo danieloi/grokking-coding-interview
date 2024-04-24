8e99272db8da61b275242da73ce468c03c4034e0
Mayowa Daniel <mayowadan17@gmail.com>
2024-04-24 23:45:47 +0100

    Refactor longestCommonSubsequence function

--- ts/13-dynamic-programming/12-longest-common-subsequence-1-recursive.ts
+++ ts/13-dynamic-programming/12-longest-common-subsequence-1-recursive.ts
@@ -15,33 +15,31 @@ function longestCommonSubsequence(str1, str2) {
   return LCSHelper(str1, str2, 0, 0, dp);
 }
 
-
-
-
-
 function LCSHelper(str1, str2, i, j, dp) {
   // If either cursor has reached the end, return 0
   if (i === str1.length || j === str2.length) {
     return 0;
-  } else if (dp[i][j] === -1) {
-    // If current characters match, move both cursors
-    if (str1[i] === str2[j]) {
-      
-      dp[i][j] = 1 + LCSHelper(str1, str2, i + 1, j + 1, dp);
-    } else {
-      // Else, choose the maximum length of two
-      // possibilities
-      dp[i][j] = Math.max(
-      // If we don't include the current character in str1
-        LCSHelper(str1, str2, i + 1, j, dp),
-      // If we don't include the current character in str2
-        LCSHelper(str1, str2, i, j + 1, dp)
-      );
-    }
   }
 
-  // Return the length of the longest 
-  // common subsequence
+  // If the current state has already been computed, return it
+  if (dp[i][j] !== -1) {
+    return dp[i][j];
+  }
+
+  // If current characters match, move both cursors
+  if (str1[i] === str2[j]) {
+    dp[i][j] = 1 + LCSHelper(str1, str2, i + 1, j + 1, dp);
+  } else {
+    // Else, choose the maximum length of two possibilities
+    dp[i][j] = Math.max(
+      // If we don't include the current character in str1
+      LCSHelper(str1, str2, i + 1, j, dp),
+      // If we don't include the current character in str2
+      LCSHelper(str1, str2, i, j + 1, dp)
+    );
+  }
+
+  // Return the length of the longest common subsequence
   return dp[i][j];
 }
 
