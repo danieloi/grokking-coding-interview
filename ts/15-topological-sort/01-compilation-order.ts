c3edeff5fe73db5ac6622e9e2eca4289db94e222
Mayowa Daniel <mayowadan17@gmail.com>
2024-05-24 12:00:51 -0700

    add compilation order problem

--- ts/15-topological-sort/01-compilation-order.ts
+++ ts/15-topological-sort/01-compilation-order.ts
@@ -1,10 +1,16 @@
 import { Queue, print2DArray } from "./utils";
 
-function findCompilationOrder(dependencies: [string, string][]) {
+function findCompilationOrder(
+  dependencies: [string, string][]
+) {
+  // the array of the compilation order we return
   let sortedOrder: string[] = [];
+  // the graph as an adjacency list
   let graph: { [key: string]: string[] } = {};
+  // the in-degree of each node
   let inDegree: { [key: string]: number } = {};
 
+  // initialize the graph and in-degree
   dependencies.forEach((x) => {
     let parent = x[1];
     let child = x[0];
@@ -15,9 +21,11 @@ function findCompilationOrder(dependencies: [string, string][]) {
   });
 
   if (Object.keys(graph).length === 0) {
+    // if the graph is empty, return an empty array
     return sortedOrder;
   }
 
+  // populate the graph and in-degree
   dependencies.forEach((dependency) => {
     let parent = dependency[1];
     let child = dependency[0];
@@ -25,20 +33,27 @@ function findCompilationOrder(dependencies: [string, string][]) {
     inDegree[child] += 1;
   });
 
+  // the queue of the nodes with in-degree 0
   let sources = new Queue();
 
+  // find the nodes with in-degree 0
   Object.keys(inDegree).forEach((key) => {
     if (inDegree[key] == 0) {
       sources.enqueue(key);
     }
   });
 
+  // process the nodes with in-degree 0
   while (sources.length != 0) {
+    // dequeue a node from the queue
     let vertex = sources.dequeue();
+    // add the node to the sorted order
     sortedOrder.push(vertex);
 
+    // decrement the in-degree of the node's children
     graph[vertex].forEach((child) => {
       inDegree[child] -= 1;
+      // when a child's in-degree is 0, enqueue it
       if (inDegree[child] == 0) {
         sources.enqueue(child);
       }
@@ -48,8 +63,24 @@ function findCompilationOrder(dependencies: [string, string][]) {
   if (sortedOrder.length != Object.keys(graph).length) {
     return [];
   }
+  // if the sorted order has all the nodes, return the
+  // sorted order
   return sortedOrder;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Driver code
 function main() {
